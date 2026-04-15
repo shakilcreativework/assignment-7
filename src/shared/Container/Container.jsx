ef1c722d3ee61ecbf92f20b1b640b7889cd652b3
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-15 19:21:55 +0600

    now footer and navbar fully complete

--- src/shared/Container/Container.jsx
+++ src/shared/Container/Container.jsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 
 const Container = ({children, className = "max-w-7xl"}) => {
     return (
-        <div className={clsx("mx-auto px-4 md:px-5 lg:px-0", className)}>
+        <div className={clsx("mx-auto px-4 md:px-5 lg:px-6 xl:px-0", className)}>
             {children}
         </div>
     );
