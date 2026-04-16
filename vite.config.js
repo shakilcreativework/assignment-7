330f55d192738fe8bcd29be69d4d665393337660
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-16 22:08:57 +0600

    now fixed deploy problem one file size make problem for recharts

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+
+  // ✅ ADD THIS PART
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("recharts")) return "recharts";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
