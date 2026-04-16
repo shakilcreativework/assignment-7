9807a67a6b38f7be1289684a47b3d288c0566446
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-16 16:01:56 +0600

    start stats codes

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Container from '../../shared/Container/Container';
 
 const Stats = () => {
     return (
         <div className=" py-10 md:py-14 lg:py-20">
-            Stats
+            <Container>
+                <div className='space-y-8'>
+                    <h1 className=' text-3xl md:text-4xl lg:text-5xl text-[#1f2937] font-bold'>Friendship Analytics</h1>
+                    <div className='bg-white rounded-lg h-20'>
+
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 };
